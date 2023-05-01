ca2ddd6294a65bdf8baaabba66750e579c1aab62
Johanna <johannarios.es@gmail.com>
2023-05-01 18:12:39 -0500

    First Comit

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Title from "../Title";
+import Account from "../Account";
+import styled from "styled-components";
+import List from "../List"
+
+const StyleContainer = styled.div`
+  background-color: ${({theme}) => theme.body};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StylesContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+
+const Container = () => {
+  return (
+    <StyleContainer>
+      <Title>Smart Bank</Title>
+      <StylesContent>
+        <Account />
+        <List/>
+      </StylesContent>
+    </StyleContainer>
+  );
+};
+
+export default Container;
